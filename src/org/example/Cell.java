9172033bd567dd654dec8f066103e287adc0faa9
Marcoindrajaya21 <162570609+Marcoindrajaya21@users.noreply.github.com>
2024-12-06 22:43:38 +0700

    Update Cell.java

--- src/org/example/Cell.java
+++ src/org/example/Cell.java
@@ -2,7 +2,7 @@
  * ES234317-Algorithm and Data Structures
  * Semester Ganjil, 2024/2025
  * Group Capstone Project
- * Group #6
+ * Group #17
  * 1 - 5026231019 - Nathaniel Lado Hadi Winata
  * 2 - 5026231031 - Marco Indrajaya
  */
